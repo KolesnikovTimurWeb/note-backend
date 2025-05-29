6164b4f4e7ab3ef855eb7197756fdde3abc40b80
Kolesnikov <kolesnikovtimyrweb@gmail.com>
2025-05-29 12:30:06 +0200

    Updated the storage route

--- models/Storage.js
+++ models/Storage.js
@@ -16,6 +16,11 @@ const storageSchema = new mongoose.Schema({
       ref: "Note",
     },
   ],
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
   createdAt: {
     type: Date,
     default: Date.now,
--- routes/storageRoute.js
+++ routes/storageRoute.js
@@ -1,17 +1,21 @@
 import express from "express";
 import Storage from "../models/Storage.js";
-
+import protectionRoute from "../middleware/auth.middleware.js";
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
 const router = express.Router();
 
-router.post("/", async (req, res) => {
+router.post("/", protectionRoute, async (req, res) => {
   try {
     const { title, image } = req.body;
-
+    const token = req.header("Authorization").replace("Bearer ", "");
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await User.findById(decoded.userId).select("-password");
     if (!title || !image) {
       return res.status(400).json({ message: "Title and image are required" });
     }
 
-    const storage = new Storage({ title, image });
+    const storage = new Storage({ title, image, user: user._id });
     await storage.save();
 
     res.status(201).json({ storage });
@@ -20,9 +24,15 @@ router.post("/", async (req, res) => {
     res.status(500).json({ message: "Server error" });
   }
 });
-router.get("/", async (req, res) => {
+router.get("/", protectionRoute, async (req, res) => {
   try {
-    const storage = await Storage.find();
+    const token = req.header("Authorization").replace("Bearer ", "");
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await User.findById(decoded.userId).select("-password");
+    if (!user) {
+      return res.status(401).json({ message: "Invalid token" });
+    }
+    const storage = await Storage.find({ user: user._id });
 
     res.status(200).json({ storage });
   } catch (err) {
